43f7f1fe3264c252a80acb2cbe85ead437f887a9
Bocek Juan <juanbocekchaco_argentina@hotmail.com>
2023-02-02 04:52:11 -0300

    crear cuneta - inicio de sesión - cierre de sesión

--- BDproductos/autenticacion/urls.py
+++ BDproductos/autenticacion/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import VistaRegistro, salir
+from .views import VistaRegistro, salir, acceder
 
 urlpatterns = [
     path('registro/', VistaRegistro.as_view(), name="registro"),
-    path('salir/', salir, name="salir")
+    path('salir/', salir, name="salir"),
+    path('acceder/', acceder, name="acceder"),
 ]
 
 
--- BDproductos/autenticacion/views.py
+++ BDproductos/autenticacion/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
+
+def acceder(request):
+    if request.method =="POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre_usuario = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre_usuario, password=password)
+            if usuario is not None:
+                login(request, usuario)
+                messages.success(request, F"Bienvenido de nuevo{nombre_usuario}")
+                return redirect("blog")
+            else:
+                messages.error(request, "los datos son incorrectos")
+        else:
+            messages.error(request, "los datos son incorrectos")
+            
+    form = AuthenticationForm()
+    return render(request, "acceder.html", {"form": form})
+
 
 class VistaRegistro(View):
     def get(self, request):
@@ -25,4 +45,5 @@ class VistaRegistro(View):
 def salir(request):
     logout(request)
     messages.success(request, f"Tu sesión se ha cerrado correctamente")
-    return redirect("blog")
+    return redirect("acceder")
+
